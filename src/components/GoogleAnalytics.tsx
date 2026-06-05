487f310c760d451df919b271f60d7472a602b9d4
Brent <bcizle@gmail.com>
2026-06-05 01:33:52 -0600

    Replace GTM with GA4 tag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,23 +1,22 @@
-import Script from "next/script";
+const gaId = "G-LHQPPQHG8X";
 
 export default function GoogleAnalytics() {
-  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  if (!gaId) return null;
-
   return (
     <>
-      <Script
+      <script
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-        strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${gaId}');
-        `}
-      </Script>
+        `,
+        }}
+      />
     </>
   );
 }
